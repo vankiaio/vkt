999381735530c9ff0fad655e70af891050daef2a
tokyoliyi <tokyoliyi@139.com>
2019-08-16 10:12:16 +0800

    fixed nodeosd.sh

--- Docker/nodeosd.sh
+++ Docker/nodeosd.sh
@@ -11,11 +11,11 @@ if [ -f '/opt/eosio/bin/data-dir/config.ini' ]; then
     cp /config.ini /opt/eosio/bin/data-dir
 fi
 
-if [ -d '/opt/eosio/bin/data-dir/contracts' ]; then
-    echo
-  else
+#if [ -d '/opt/eosio/bin/data-dir/contracts' ]; then
+#    echo
+#  else
 #    cp -r /contracts /opt/eosio/bin/data-dir
-fi
+#fi
 
 while :; do
     case $1 in
